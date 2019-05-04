63a75175e1f16be49f1010e3069006a5d8118dd4
heuzin <matheusqs.arq@gmail.com>
2019-05-03 21:40:45 -0400

    Added mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+
+let button = document.querySelector('.btn');
+button.addEventListener('click', function (){
+    button.style.color = 'red';
+})
+
+let funBus = document.querySelector('.footer');
+funBus.addEventListener('keydown', function(){
+    funBus.style.textShadow = "5px 5px 1px #ff0000, 10px 10px 1px #0000ff";
+})
